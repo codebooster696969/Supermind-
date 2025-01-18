58b144549ae448230504dd38fdf876ccb9a14b0d
Shashank Singh <shashanksgh3@gmail.com>
2025-01-19 01:07:57 +0530

    Port update

--- server/index.js
+++ server/index.js
@@ -5,7 +5,9 @@ const cors = require('cors');
 const app = express();
 
 // Enable CORS for all origins (or restrict to your frontend origin)
-app.use(cors({}));
+app.use(cors({
+  origin: 'http://localhost:5173', // Replace with your frontend URL
+}));
 
 // Middleware to parse JSON request bodies
 app.use(express.json());
